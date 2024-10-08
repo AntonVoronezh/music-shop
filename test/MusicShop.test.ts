e0f84ed0c3c60be4887b0ec24d65b12866b62fc4
AntonVoronezh <pstut@mail.ru>
2024-10-08 19:29:59 +0300

    4

--- test/MusicShop.test.ts
+++ test/MusicShop.test.ts
@@ -12,4 +12,27 @@ describe("MusicShop", function () {
     return { shop, owner, buyer };
   }
 
+  it("should allow to add  albums", async function () {
+    const { shop } = await loadFixture(deploy);
+
+    const title = "Demo";
+    const price = 100;
+    const uid = ethers.solidityPackedKeccak256(["string"], [title]);
+    const qty = 5;
+    const initialIndex = 0;
+
+    const addTx = await shop.addAlbum(uid, title, price, qty);
+    await addTx.wait();
+
+    const album  = await shop.albums(initialIndex);
+
+    expect(album.index).to.eq(initialIndex);
+    expect(album.uid).to.eq(uid);
+    expect(album.price).to.eq(price);
+    expect(album.quantity).to.eq(qty);
+
+    expect(await shop.currentIndex()).to.eq(initialIndex + 1);
+  });
+
+
 });
