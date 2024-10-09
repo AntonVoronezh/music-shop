3c3b73821c5582ecdc55508308792c7ea6a999bf
AntonVoronezh <pstut@mail.ru>
2024-10-09 17:11:14 +0300

    4

--- test/MusicShop.test.ts
+++ test/MusicShop.test.ts
@@ -34,5 +34,33 @@ describe("MusicShop", function () {
     expect(await shop.currentIndex()).to.eq(initialIndex + 1);
   });
 
+  it("should allow to buy  albums", async function () {
+    const { shop, buyer } = await loadFixture(deploy);
 
+    const title = "Demo";
+    const price = 100;
+    const uid = ethers.solidityPackedKeccak256(["string"], [title]);
+    const qty = 5;
+    const albumIdxToBuy = 0;
+    const initialOrderId = 0;
+
+    expect(await shop.currentOrderId()).to.eq(initialOrderId);
+
+    const addTx = await shop.addAlbum(uid, title, price, qty);
+    await addTx.wait();
+
+    const album = await shop.albums(albumIdxToBuy);
+
+    const buyTx = await shop.connect(buyer).buy(albumIdxToBuy, {value: price});
+    const receipt = await buyTx.wait();
+
+    await expect(buyTx).changeEtherBalances([shop, buyer], [price, -price]);
+
+    expect(receipt).not.eq(undefined);
+
+    const block = await ethers.provider.getBlock(receipt!.blockNumber);
+    const expectTimeStamp = block?.timestamp;
+
+    await expect(buyTx).to.emit(shop, "AlbumBought").withArgs(uid, buyer.address, expectTimeStamp);
+  });
 });
